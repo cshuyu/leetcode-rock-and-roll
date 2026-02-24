4df8a593464e5da346aa2ba561da039d6d35b890
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:46:50 -0800

    Create 496-NextGreaterElementI

---
+++ 496-NextGreaterElementI/496-NextGreaterElementI.java
@@ -0,0 +1,35 @@
+// Last updated: 2/23/2026, 6:46:49 PM
+public class Solution {
+    public int[] nextGreaterElement(int[] findNums, int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        for(int n: nums){
+            if(stack.isEmpty() || stack.peek() >= n){
+                stack.push(n);
+            }
+            else{
+               while(!stack.isEmpty() && stack.peek() < n){
+                   map.put(stack.pop(), n);
+               }
+               
+               stack.push(n);
+            }
+        }
+        
+        int[] ret = new int[findNums.length];
+        
+        for(int i = 0; i < findNums.length; i++){
+            int num = findNums[i];
+            if(map.containsKey(num)){
+                ret[i] = map.get(num);
+            }
+            else{
+                ret[i] = -1;
+            }
+        }
+        
+        return ret;
+    }
+}
