437023e4faf0406c99e66c6ff2d3910b53adc4bf
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:46:38 -0800

    Create 520-DetectCapital

---
+++ 520-DetectCapital/520-DetectCapital.java
@@ -0,0 +1,13 @@
+// Last updated: 2/23/2026, 6:46:37 PM
+public class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word == null) return false;
+        if(word.length() == 0) return true;
+        
+        String upper = new String(word).toUpperCase();
+        String lower = new String(word).toLowerCase();
+        String cap = word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase();
+        
+        return word.equals(upper) || word.equals(lower) || word.equals(cap);
+    }
+}
