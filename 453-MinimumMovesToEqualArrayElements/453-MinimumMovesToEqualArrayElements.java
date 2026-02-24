d51098c0e1c49b5dab6ae34294dbc4b461c1b515
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:47:12 -0800

    Create 453-MinimumMovesToEqualArrayElements

---
+++ 453-MinimumMovesToEqualArrayElements/453-MinimumMovesToEqualArrayElements.java
@@ -0,0 +1,14 @@
+// Last updated: 2/23/2026, 6:47:12 PM
+public class Solution {
+    public int minMoves(int[] nums) {
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        
+        for(int num: nums){
+            sum += num;
+            min = Math.min(min, num);
+        }
+        
+        return sum - min * nums.length;
+    }
+}
