ae1c0bf2b987119ec7ea51311c655ad20a6823d4
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:46:59 -0800

    Create 477-TotalHammingDistance

---
+++ 477-TotalHammingDistance/477-TotalHammingDistance.java
@@ -0,0 +1,26 @@
+// Last updated: 2/23/2026, 6:46:59 PM
+public class Solution {
+    public int totalHammingDistance(int[] nums) {
+        int count = 0;
+        
+        if(nums == null || nums.length <= 1){
+            return 0;
+        }
+        
+        int size = nums.length;
+        
+        for(int i = 0; i < 32; i++){
+            int bitCount = 0;
+            for(int j = 0; j < size; j++){
+                int curr = nums[j];
+                bitCount += ((curr >> i) & 1);
+            }
+            
+            count += bitCount * (size - bitCount);
+        }
+        
+        
+        
+        return count;
+    }
+}
