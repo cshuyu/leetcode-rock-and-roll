55d6a3b5ad56e7051a8db97ff7c5be2cb10e3c07
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:46:51 -0800

    Create 495-TeemoAttacking

---
+++ 495-TeemoAttacking/495-TeemoAttacking.java
@@ -0,0 +1,24 @@
+// Last updated: 2/23/2026, 6:46:50 PM
+public class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        if(duration <= 0 || timeSeries == null || timeSeries.length == 0) return 0;
+        
+        int count = 0;
+        int end = 0;
+        int prev = -1;
+        
+        for(int start : timeSeries){
+            if(start >= end){
+                count += duration;
+                end = start + duration;
+                prev = start;
+            }
+            else{
+               count += start - prev;
+               end = start + duration;
+               prev = start;
+            }
+        }
+        return count;
+    }
+}
