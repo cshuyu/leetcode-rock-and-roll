58a8fb08ed0cac52b293e5c6d67c2682a1feb691
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:46:57 -0800

    Create 484-FindPermutation

---
+++ 484-FindPermutation/484-FindPermutation.java
@@ -0,0 +1,37 @@
+// Last updated: 2/23/2026, 6:46:57 PM
+public class Solution {
+    public int[] findPermutation(String s) {
+       int size = s.length();
+       
+       int[] array = new int[size + 1];
+       
+       for(int i = 0; i < array.length; i++){
+           array[i] = i + 1;
+       }
+       
+       int index = 0;
+       
+       while(index < s.length()){
+           if(s.charAt(index) == 'I'){
+               index++;
+           }
+           else{
+               int start = index;
+               while(index < s.length() && s.charAt(index) == 'D'){
+                   index++;
+               }
+               int end = index;
+               
+               while(start < end){
+                   int temp = array[start];
+                   array[start] = array[end];
+                   array[end] = temp;
+                   start++;
+                   end--;
+               }
+           }
+       }
+       
+       return array;
+    }
+}
