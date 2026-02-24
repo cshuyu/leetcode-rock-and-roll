83da82e02ddb7565cb5631348c189a51d88fba73
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:44:40 -0800

    Create 1371-MinimumRemoveToMakeValidParentheses

---
+++ 1371-MinimumRemoveToMakeValidParentheses/1371-MinimumRemoveToMakeValidParentheses.py
@@ -0,0 +1,22 @@
+# Last updated: 2/23/2026, 6:44:39 PM
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            elif char == ')':
+                if stack and s[stack[-1]] == '(':
+                    stack.pop()
+                else:
+                    stack.append(i)
+        
+        result = []
+        for i, char in enumerate(s):
+            if stack and i == stack[0]:
+                stack.pop(0)
+                continue
+            result.append(char)
+        
+        return ''.join(result)
+
