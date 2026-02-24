c9620ed2f50de6ac3bea999d690dfaec1f31597e
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:47:11 -0800

    Create 454-4sumIi

---
+++ 454-4sumIi/454-4sumIi.java
@@ -0,0 +1,25 @@
+// Last updated: 2/23/2026, 6:47:11 PM
+public class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        int count = 0;
+        
+        for(int i = 0; i < A.length; i++){
+            for(int j = 0; j < B.length; j++){
+                int sum = A[i] + B[j];
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+        
+        
+        for(int i = 0; i < C.length; i++){
+            for(int j = 0; j < D.length; j++){
+                int sum = C[i] + D[j];
+                count += map.getOrDefault( -1 * sum, 0);
+            }
+        }
+        
+        return count;
+    }
+}
