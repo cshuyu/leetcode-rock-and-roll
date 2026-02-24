a0f9af1cd65daf5ff1e7b52b08fb0cc3de83d3ff
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:47:05 -0800

    Create 462-MinimumMovesToEqualArrayElementsIi

---
+++ 462-MinimumMovesToEqualArrayElementsIi/462-MinimumMovesToEqualArrayElementsIi.java
@@ -0,0 +1,18 @@
+// Last updated: 2/23/2026, 6:47:05 PM
+public class Solution {
+    public int minMoves2(int[] nums) {
+        int ret = 0;
+        int l = 0;
+        int r = nums.length -1 ;
+        
+        Arrays.sort(nums);
+        
+        while(l < r){
+            ret += nums[r] - nums[l];
+            l++;
+            r--;
+        }
+        
+        return ret;
+    }
+}
