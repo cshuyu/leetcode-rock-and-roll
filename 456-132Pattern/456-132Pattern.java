5a36cead8bb42c6319423396a4da451dd06b5f27
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:47:09 -0800

    Create 456-132Pattern

---
+++ 456-132Pattern/456-132Pattern.java
@@ -0,0 +1,47 @@
+// Last updated: 2/23/2026, 6:47:09 PM
+public class Solution {
+    public boolean find132pattern(int[] nums) {
+        Stack<Interval> stack = new Stack<Interval>();
+        
+        
+        for(int num : nums){
+            if(stack.isEmpty() || stack.peek().min > num){
+                stack.push(new Interval(num, num));
+            }
+            else if(stack.peek().min < num){
+                Interval top = stack.pop();
+                
+                if(num < top.max){
+                    return true;
+                }
+                else{
+                    top.max = num;
+                    
+                    while(!stack.isEmpty() && stack.peek().max <= top.max){
+                        stack.pop();
+                    }
+                    
+                    if(!stack.isEmpty() && stack.peek().min < num) return true;
+                    
+                    stack.push(top);
+                }
+                
+                
+            }
+        }
+        
+        return false;
+        
+        
+    }
+    
+    class Interval{
+        int min;
+        int max;
+        
+        public Interval(int min, int max){
+            this.min = min;
+            this.max = max;
+        }
+    }
+}
