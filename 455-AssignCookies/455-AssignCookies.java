60d432320fee65201a0624a76c01911b40abf2b6
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:47:10 -0800

    Create 455-AssignCookies

---
+++ 455-AssignCookies/455-AssignCookies.java
@@ -0,0 +1,27 @@
+// Last updated: 2/23/2026, 6:47:10 PM
+public class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+    
+        int ret = 0;
+        int kidIndex = 0;
+        int cookieIndex = 0;
+        
+        while(kidIndex < g.length && cookieIndex < s.length){
+            int demand = g[kidIndex];
+            int supply = s[cookieIndex];
+            
+            if(supply >= demand){
+                ret++;
+                kidIndex++;
+                cookieIndex++;
+            }
+            else{
+                cookieIndex++;
+            }
+        }
+        
+        return ret;
+    }
+}
