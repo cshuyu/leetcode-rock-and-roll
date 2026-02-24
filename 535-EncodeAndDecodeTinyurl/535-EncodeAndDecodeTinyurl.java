8c541cc7b0ad9e0aff759f61a778801bbf275ec1
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:46:33 -0800

    Create 535-EncodeAndDecodeTinyurl

---
+++ 535-EncodeAndDecodeTinyurl/535-EncodeAndDecodeTinyurl.java
@@ -0,0 +1,19 @@
+// Last updated: 2/23/2026, 6:46:33 PM
+public class Codec {
+    
+    private HashMap<Integer, String> map = new HashMap<Integer, String>();
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        map.put(longUrl.hashCode(), longUrl);
+        return "http://tinyurl.com/" + longUrl.hashCode();
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return map.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", "")));
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
