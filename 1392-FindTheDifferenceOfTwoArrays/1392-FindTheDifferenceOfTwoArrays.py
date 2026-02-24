0bd5bcb3c674464758917692e2532bba291e8c4c
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:44:39 -0800

    Create 1392-FindTheDifferenceOfTwoArrays

---
+++ 1392-FindTheDifferenceOfTwoArrays/1392-FindTheDifferenceOfTwoArrays.py
@@ -0,0 +1,13 @@
+# Last updated: 2/23/2026, 6:44:38 PM
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        s1 = set(nums1)
+        s2 = set(nums2)
+        ans = [[], []]
+        for i in s1:
+            if i not in s2:
+                ans[0].append(i)
+        for j in s2:
+            if j not in s1:
+                ans[1].append(j)
+        return ans
