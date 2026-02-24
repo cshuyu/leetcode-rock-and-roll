358d302b41ac88d1af6555ee8dca79d9fb9a766a
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:47:06 -0800

    Create 461-HammingDistance

---
+++ 461-HammingDistance/461-HammingDistance.java
@@ -0,0 +1,14 @@
+// Last updated: 2/23/2026, 6:47:06 PM
+public class Solution {
+    public int hammingDistance(int x, int y) {
+        int count = 0;
+        int xor = x ^ y;
+        
+        for(int i = 0; i < 32; i++){
+            if((xor & 1) == 1) count++;
+            xor = xor >>> 1;
+        }
+        
+        return count;
+    }
+}
