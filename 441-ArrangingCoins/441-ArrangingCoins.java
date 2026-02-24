260b5c3899c743c18697111c4b5ab9f4e2f1485e
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:47:20 -0800

    Create 441-ArrangingCoins

---
+++ 441-ArrangingCoins/441-ArrangingCoins.java
@@ -0,0 +1,8 @@
+// Last updated: 2/23/2026, 6:47:20 PM
+public class Solution {
+    public int arrangeCoins(int n) {
+        
+        return (int)((-1 + Math.sqrt(1 + 8 * (long)n)) / 2);
+        
+    }
+}
