c80b20497e0048212ccf28a1d72d8d85718a9727
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:46:51 -0800

    Create 494-TargetSum

---
+++ 494-TargetSum/494-TargetSum.java
@@ -0,0 +1,32 @@
+// Last updated: 2/23/2026, 6:46:51 PM
+public class Solution {
+    public int findTargetSumWays(int[] nums, int S) {
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+        
+        ret.add(0);
+        
+        helper(nums, 0, 0, S, ret);
+        
+        return ret.get(0);
+    }
+    
+    
+    private void helper(int[] nums, int index, int sum, int target, ArrayList<Integer> ret){
+        if(index == nums.length){
+            if(sum == target){
+                ret.set(0, ret.get(0) + 1);
+                return;
+            }
+            else{
+                return;
+            }
+        }
+        
+        int positive = sum + nums[index];
+        helper(nums, index + 1, positive, target, ret);
+        
+        int negative = sum - nums[index];
+        helper(nums, index + 1, negative, target, ret);
+        
+    }
+}
