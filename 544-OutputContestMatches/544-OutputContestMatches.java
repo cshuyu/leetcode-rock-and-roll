c43910c553b0ff64c8e3708cfbcc09b9fd7e770b
Shuyu Chen <2011cshuyu@gmail.com>
2026-02-23 18:46:24 -0800

    Create 544-OutputContestMatches

---
+++ 544-OutputContestMatches/544-OutputContestMatches.java
@@ -0,0 +1,25 @@
+// Last updated: 2/23/2026, 6:46:24 PM
+public class Solution {
+    public String findContestMatch(int n) {
+        HashMap<Integer, String> map = new HashMap<Integer,String>();
+        boolean firstRound = true;
+        
+        while(n > 1){
+            int mid = n / 2;
+            
+            for(int i = 1; i <= mid; i++){
+                if(firstRound){
+                    map.put(i, "(" + i + "," + (n + 1 - i) + ")");
+                }
+                else{
+                    map.put(i, "(" + map.get(i) + "," + map.get(n + 1 - i) +")");
+                }
+            }
+            firstRound = false;
+            
+            n = n / 2;
+        }
+        
+        return map.get(1);
+    }
+}
