9278d7ea3f60aee4b3160011d3ca3e680f2d6dad
Shuyu Chen <2011cshuyu@gmail.com>
2026-04-01 17:27:24 -0700

    Create 83-RemoveDuplicatesfromSortedList

---
+++ 83-RemoveDuplicatesfromSortedList/version/python/83-RemoveDuplicatesfromSortedList_20260401_172720.py
@@ -0,0 +1,13 @@
+# Last updated: 4/1/2026, 5:27:20 PM
+# Two Pointers: Reverse String
+1class Solution:
+2    def reverseString(self, s: List[str]) -> None:
+3        """
+4        Do not return anything, modify s in-place instead.
+5        """
+6        left = 0
+7        right = len(s)-1
+8        while left<right:
+9            s[left], s[right] = s[right], s[left]
+10            left += 1
+11            right -= 1
