6f447decc9cd92679374f9eb761eb6003566a3b3
Shuyu Chen <2011cshuyu@gmail.com>
2026-05-18 13:00:09 -0700

    Create 347-TopKFrequentElements

---
+++ 347-TopKFrequentElements/version/python/347-TopKFrequentElements_20260518_130009.py
@@ -0,0 +1,25 @@
+# Last updated: 5/18/2026, 1:00:09 PM
+1'''
+2Time Complexity: O(nlogn)
+3we limit the heap size to be k, which can improve the complexity to nlogk
+4Space Complexity: O(n)
+5'''
+6class Solution:
+7    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+8        freqMap = defaultdict(int)
+9        minHeap = []
+10        res = []
+11
+12        for num in nums:
+13            freqMap[num] += 1
+14
+15        for key in freqMap:
+16            heapq.heappush(minHeap, (freqMap[key], key))
+17            while len(minHeap) > k:
+18                heapq.heappop(minHeap)
+19        
+20        for _,key in minHeap:
+21            res.append(key)
+22
+23        return res
+24
