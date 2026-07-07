912a5c9fac641815c93192b16dbcf5f27a67f0eb
Shuyu Chen <2011cshuyu@gmail.com>
2026-07-06 17:02:38 -0700

    Create 239-SlidingWindowMaximum

---
+++ 239-SlidingWindowMaximum/version/python/239-SlidingWindowMaximum_20260706_170237.py
@@ -0,0 +1,32 @@
+# Last updated: 7/6/2026, 5:02:37 PM
+1"""
+2sliding window + monotonic queue
+3time: O(n)
+4space: O(n)
+5"""
+6class Solution:
+7    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+8        dq = deque()
+9        res = []
+10        left=right=0
+11        while right<len(nums):
+12            while dq and nums[dq[-1]]<nums[right]:
+13                dq.pop()
+14            dq.append(right)
+15            right += 1
+16
+17            while right-left>k:
+18                if dq[0]<=left:
+19                    dq.popleft()
+20                left += 1
+21            
+22            if right-left==k:
+23                res.append(nums[dq[0]])
+24        return res
+25
+26
+27
+28
+29
+30
+31
