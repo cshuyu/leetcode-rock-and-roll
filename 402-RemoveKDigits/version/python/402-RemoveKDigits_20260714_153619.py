81af187b443c475ee7b30f726aec97ec7a7b72b1
Shuyu Chen <2011cshuyu@gmail.com>
2026-07-14 15:36:19 -0700

    Create 402-RemoveKDigits

---
+++ 402-RemoveKDigits/version/python/402-RemoveKDigits_20260714_153619.py
@@ -0,0 +1,20 @@
+# Last updated: 7/14/2026, 3:36:19 PM
+1'''
+2Time and Space complexity are both O(n)
+3'''
+4class Solution:
+5    def removeKdigits(self, num: str, k: int) -> str:
+6        stack = []
+7
+8        for i in range(len(num)):
+9            while stack and stack[-1] > num[i] and k>0:
+10                stack.pop()
+11                k -= 1
+12            stack.append(num[i])
+13
+14        while k>0:
+15            stack.pop()
+16            k -= 1
+17        
+18        res = "".join(stack).lstrip('0')
+19        return res if res else "0"
