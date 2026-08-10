3f0201e638881cbac50d6c7fafcc66b694f47146
Shuyu Chen <2011cshuyu@gmail.com>
2026-08-10 15:46:41 -0700

    Create 772-BasicCalculatorIII

---
+++ 772-BasicCalculatorIII/version/python/772-BasicCalculatorIII_20260810_154641.py
@@ -0,0 +1,53 @@
+# Last updated: 8/10/2026, 3:46:41 PM
+# Calculator Design: stack
+1"""
+2Go through the string:
+3meet the digits, transfer to the num:
+4 num = num*10+int(char)
+5
+6meet the signs, we need to analyze case by case:
+7 prev_sign == "+-"  ==> stack.append(sign*num) until we meet the next sign and update the prev sign, num=0.
+8 prev_sign == "*/" ==> stack.pop() * or / num, append it into the stack and update the prev sign, num=0.
+9 if it is the last element ==> calculate prev_sign and num, append into the stack.
+10
+11parentheses:
+12 ch == "(" ==> i+1 call helper() function to calculate the inside of ()
+13 ch == ")" ==> return sum(stack), transfer the string in prenthese to a num, which is same as digit num
+14 """
+15class Solution:
+16    def calculate(self, s: str):
+17        i = 0
+18
+19        def helper():
+20            nonlocal i
+21            stack = []
+22            num = 0
+23            prev_sign = "+"
+24
+25            while i<len(s):
+26                ch = s[i]
+27                if ch.isdigit():
+28                    num = num*10+int(s[i])
+29                if ch == "(":
+30                    i += 1
+31                    num = helper()
+32                if ch in "+-*/)" or i==len(s)-1:
+33                    if prev_sign == "+":
+34                        stack.append(num)
+35                    elif prev_sign == "-":
+36                        stack.append(-num)
+37                    elif prev_sign == "*":
+38                        stack.append(stack.pop()*num)
+39                    elif prev_sign == "/":
+40                        stack.append(int(stack.pop()/num))
+41
+42                    if ch == ")":
+43                        return sum(stack)
+44                    
+45                    prev_sign = ch
+46                    num = 0
+47
+48                i += 1
+49            return sum(stack)
+50    
+51        return helper()
